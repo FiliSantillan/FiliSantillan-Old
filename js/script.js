ea5a579b6a6ab563e59a68b61e6642ae8f5d3203
Fili Martinez <fili06604@gmail.com>
2014-08-23 03:33:53 -0500

    Empezando con Desktop :D
    - Imagen de fondo
    -Estilos y más

--- js/script.js
+++ js/script.js
@@ -1,13 +1,4 @@
 $(document).ready(function(){
-	onscroll = function(){
-		var headerTransparency = $(document).scrollTop();
-		if (headerTransparency >= 106){
-		$('.header').addClass('MainMenu-scrolled');
-		}
-		else {
-			$('.header').removeClass('MainMenu-scrolled');
-		}
-	}
 	var target = null;
 	$('.image').hover(function(e){
 		target = $(this);
@@ -26,6 +17,18 @@ $(document).ready(function(){
 	$(function(){
 	//clic en un enlace de la lista
 	$('ul li a').on('click',function(e){
+		//prevenir en comportamiento predeterminado del enlace
+		e.preventDefault();
+		//obtenemos el id del elemento en el que debemos posicionarnos
+		var strAncla=$(this).attr('href');
+		
+		//utilizamos body y html, ya que dependiendo del navegador uno u otro no funciona
+		$('body,html').stop(true,true).animate({
+			//realizamos la animacion hacia el ancla
+			scrollTop: $(strAncla).offset().top
+		},1000);
+	});
+	$('.flechita').on('click',function(e){
 		//prevenir en comportamiento predeterminado del enlace
 		e.preventDefault();
 		//obtenemos el id del elemento en el que debemos posicionarnos
